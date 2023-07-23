2ea3b9b07278a3cd25e5522d5f60b1ad00244029
Qihan <33321809+huangqihanssh@users.noreply.github.com>
2023-07-23 19:53:08 +0800

    Updated AHP Tree

--- src/pages/TableList/components/AddForm.tsx
+++ src/pages/TableList/components/AddForm.tsx
@@ -1,17 +1,12 @@
+// @ts-nocheck
 import {
-  ProFormDateTimePicker,
   ProFormSelect,
   ProFormText,
   ProFormTextArea,
   StepsForm,
 } from '@ant-design/pro-components';
-import {FormattedMessage, useIntl} from '@umijs/max';
-import {Modal} from 'antd';
-import React from 'react';
-
-import { DownOutlined } from '@ant-design/icons';
-import { Tree } from 'antd';
-import type { DataNode, TreeProps } from 'antd/es/tree';
+import {Modal,Tree} from 'antd';
+import React, {useState} from 'react';
 
 
 export type FormValueType = {
@@ -29,62 +24,78 @@ export type UpdateFormProps = {
   values: Partial<API.RuleListItem>;
 };
 
-const treeData: DataNode[] = [
-  {
-    title: '评价体系',
-    key: '0-0',
-    children: [
-      {
-        title: '生态环境',
-        key: '0-0-0',
-        children: [
-          {
-            title: '滨水植被覆盖率与层次',
-            key: '0-0-0-0',
-          },
-          {
-            title: '生物多样性',
-            key: '0-0-0-1',
-          },
-          {
-            title: '植物的乡土性',
-            key: '0-0-0-2',
-          },
-        ],
-      },
-      {
-        title: '景观环境',
-        key: '0-0-1',
-        children: [
-          {
-            title: '景观小品美观度',
-            key: '0-0-1-0',
-          },
-        ],
-      },
-      {
-        title: '历史文化',
-        key: '0-0-2',
-        children: [
-          {
-            title: '历史文化延续性',
-            key: '0-0-2-0',
-          },
-          {
-            title: '科普教育程度',
-            key: '0-0-2-1',
-          },
-        ],
-      },
-    ],
-  },
-];
+
+const TreeNode = Tree.TreeNode
 
 const AddForm: React.FC<UpdateFormProps> = (props) => {
-  const intl = useIntl();
-  const onSelect: TreeProps['onSelect'] = (selectedKeys, info) => {
-    console.log('selected', selectedKeys, info);
-  };
+
+  const [systemName, setSystemName] = useState('')
+  const [criterions, setCriterions] = useState('')
+
+
+  // 定义节点类
+  class Node {
+    constructor(name) {
+      this.name = name;
+      this.children = [];
+    }
+  }
+
+  const parse = (text) => {
+    // 分割为分支
+    const branches = text.split(';');
+
+    // 定义根节点
+    const root = new Node(systemName);
+
+    // 遍历每个分支
+    branches.forEach(branch => {
+      // 分隔节点
+      const items = branch.split(':');
+      const parent = new Node(items[0].trim());
+      if (items.length > 1) {
+        // 有子节点,继续分割
+        items[1].split(',').forEach(item => {
+          const child = new Node(item.trim());
+          parent.children.push(child);
+        });
+      }
+      // 加入到根节点children中
+      root.children.push(parent);
+    });
+
+    return root;
+  }
+
+  const RecursiveTree = (values) => {
+    console.log(values)
+    const data = values.criterions;
+    const systemName = values.systemName;
+    const renderTreeNodes = (nodes) => {
+      if (!nodes) return null;
+      // console.log(nodes)
+      return nodes.map(node => {
+        if (node.children.length > 0) {
+          return (
+            <TreeNode title={node.name} key={node.name} disableCascade>
+              {renderTreeNodes(node.children)}
+            </TreeNode>
+          );
+        }
+
+        return <TreeNode title={node.name} key={node.name} />
+      })
+    }
+
+    return (
+      <Tree defaultExpandAll={true} showLine={true} expandAction="expand" >
+        <TreeNode title={systemName}>
+          {renderTreeNodes(data.children)}
+        </TreeNode>
+      </Tree>
+    )
+  }
+
   return (
     <StepsForm
       stepsProps={{
@@ -111,66 +122,86 @@ const AddForm: React.FC<UpdateFormProps> = (props) => {
     >
       <StepsForm.StepForm
         initialValues={{
-          name: props.values.name,
-          desc: props.values.desc,
+          systemName: props.values.systemName,
+          systemDesc: props.values.systemDesc,
         }}
         title={'基本信息'}
+        onFinish={values => {
+          console.log(values)
+          setSystemName(values.systemName)
+          return Promise.resolve(true);
+        }}
       >
         <ProFormText
-          name="name"
+          name="systemName"
           label={'评价体系名称'}
           width="md"
           rules={[
             {
-              required: true,
+              // required: true,
               message: "请输入评价体系名称！",
             },
           ]}
         />
         <ProFormTextArea
-          name="desc"
+          name="systemDesc"
           width="md"
           label={'评价体系名称'}
           placeholder={'请输入至少五个字符'}
           rules={[
             {
               required: false,
-              message: (
-                <FormattedMessage
-                  id="pages.searchTable.updateForm.ruleDesc.descRules"
-                  defaultMessage="请输入至少五个字符的规则描述！"
-                />
-              ),
-              min: 5,
+              message: '请输入规则描述！',
             },
           ]}
         />
       </StepsForm.StepForm>
       <StepsForm.StepForm
         initialValues={{
-          target: '0',
           template: '0',
+          target: '0',
+        }}
+        onFinish={values => {
+          const critText = values.critText
+          const root = parse(critText);
+          console.log(JSON.stringify(root, null, 2));
+          setCriterions(root)
+          return Promise.resolve(true);
         }}
         title={'决策模型'}
       >
         <ProFormSelect
           name="template"
           width="md"
-          label={'模板选择：'}
+          label={'模板选择'}
           valueEnum={{
             0: '无需模板',
             1: '测试模板一',
             2: '测试模板二',
           }}
         />
-        准则层配置：
-        <Tree
-          showLine
-          switcherIcon={<DownOutlined />}
-          defaultExpandedKeys={['0-0-0']}
-          onSelect={onSelect}
-          treeData={treeData}
+
+        <ProFormTextArea
+          name="critText"
+          width="lg"
+          fieldProps={{
+            rows: 8,
+          }}
+          label={'准则层配置'}
+          placeholder={'在文本输入区域中，可以定义一个新的层次结构。\n注意: 输入区分大小写。'}
+          tooltip={'\n' +
+            '注意：父节点后面跟着冒号(:)，子节点用逗号分隔(,)，每个分支必须用分号结束(;)。类别和子类别的名称必须是唯一的。不允许使用数字作为类别名称。一个类别不能仅有单个子类别。输入区分大小写。'}
+          rules={[
+            {
+              required: false,
+              message: "",
+            },
+          ]}
         />
+        配置示例如下:<br/>
+        Quality: Product Variety, Product Quality Features, Production Quality;
+        Reliability: Management & Organization, References, Capital, Annual Turnover;
+        Service: Communication, Delivery Lead Time, Customization Capability
       </StepsForm.StepForm>
       <StepsForm.StepForm
         initialValues={{
@@ -179,7 +210,7 @@ const AddForm: React.FC<UpdateFormProps> = (props) => {
         }}
         title={'指标评分'}
       >
-
+      <RecursiveTree criterions={criterions} systemName={systemName}/>
       </StepsForm.StepForm>
     </StepsForm>
   );
